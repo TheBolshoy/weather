c20a84b758576dd65c859f447fa0b5ea1f0cbe71
TheBolshoy <d-costa@mail.ru>
2025-02-25 17:25:37 +0300

    weather with classes

--- coockie.js
+++ coockie.js
@@ -1,12 +1,18 @@
 let arrLocalStorge = (localStorage.getItem('city')).split(',')
 
-function setSessionStorage() {
+function coockieSetter() {
     document.cookie = `city=${arrLocalStorge[arrLocalStorge.length - 1]} ; max-age=36` 
 }
 
-setSessionStorage()
+coockieSetter()
 
 
  
+/*
+
+
 let nameCity = arrLocalStorge[arrLocalStorge.length - 1]
 Cookies.get('name', nameCity, {expires: 1})
+
+
+*/
